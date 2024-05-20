55cd97f5614b454ac9ac358544ffaf7b0ab98a4f
EsttherAraujo <estther-araujo@hotmail.com>
2024-05-20 17:53:39 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,39 @@
+
+let palavra;
+function setup() {
+  createCanvas(400, 400);
+  
+  let palavras= ["Respeito","Educação","Esperança"];
+  palavra= random(palavras);
+  
+  
+  
+}
+function inicializaCores(){
+   background("white");
+  fill ("black");
+  textSize(64);
+  textAlign(CENTER,CENTER);
+  
+  
+}
+
+
+
+function draw() {
+  inicializaCores();
+  
+  
+  
+  
+  let maximo= width;
+  let minimo= 0;
+  
+  let quantidade= map (mouseX,0, width,1, palavra.length);
+  let parcial= palavra.substring (0, quantidade);
+  text(parcial,200,200);
+  
+  
+  
+  
+}
